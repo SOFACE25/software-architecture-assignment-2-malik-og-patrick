8ca06777867f0643b9f2af0ab764ba8dc7e17664
Patrick <202407543@post.au.dk>
2025-02-07 17:04:34 +0100

    commit

--- wargame_cpp/src/soldier.cpp
+++ wargame_cpp/src/soldier.cpp
@@ -1,12 +1,14 @@
 #include "soldier.hpp"
 
-Soldier::Soldier(int health, int damage) : Unit(health), damage(damage)
-{
-}
+Soldier::Soldier(int health, int damage) : Unit(health), damage(damage) {}
 
 void Soldier::attack(Unit &target)
 {
+    target.take_hit(damage); // Soldier inflicts damage on target
 }
+
 void Soldier::heal(Unit &target)
 {
+    // Soldiers cannot heal, so we leave this empty.
 }
+
